c181aec0e40a025287187ca4afa1df175c4fd427
Xardas <xardas.phys@gmail.com>
2025-03-27 07:20:45 -0400

    Fix linting errors

--- service/routes.py
+++ service/routes.py
@@ -38,6 +38,7 @@ def index():
 ######################################################################
 # CREATE A NEW ACCOUNT
 ######################################################################
+
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
     """
@@ -57,6 +58,7 @@ def create_accounts():
         status.HTTP_201_CREATED,
         {"Location": location_url}
     )
+
 
 ######################################################################
 # LIST ALL ACCOUNTS
@@ -91,7 +93,7 @@ def get_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(
-            status.HTTP_404_NOT_FOUND, 
+            status.HTTP_404_NOT_FOUND,
             f"Account with id [{account_id}] could not be found."
         )
     return account.serialize(), status.HTTP_200_OK
